dae5e6eb1c7df08d6a408885efd05b9e74d8f021
hypnopompicindex <hypnopompicindex@gmail.com>
2016-10-01 18:01:04 -0400

    Adds URL for SoloDetailView

--- jmad/jmad/urls.py
+++ jmad/jmad/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from solos.views import index
+from solos.views import index, SoloDetailView
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^solos/(?P<pk>\d+)/$', SoloDetailView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
